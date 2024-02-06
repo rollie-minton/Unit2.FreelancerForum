843e499682144c790a89bfdea5a0692ecbab9b9c
rollie <rollieminton@outlook.com>
2024-02-06 18:49:42 -0500

    Partially working solution

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // === State ===
 // Declare arrays to contain all possible names, occupations, and rates for freelancers
-const names = ["Alice", "Bob", "Carol", "Orion", "Nicholas", "Lester", "Arun", "Ian", "Gavriel", "Otto", "William", "Kyle", "Easi"];
-const occupations = ["Writer", "Teacher", "Programmer"];
-const prices = [30, 40, 50, 60, 70, 80, 90, 100]
+const names = ["Alice", "Bob", "Carol", "Orion", "Nicholas", "Lester", "Arun", "Ian", "Gavriel", "Otto", "William", "Kyle", "Easi",];
+const occupations = ["Writer", "Teacher", "Programmer",];
+
 // Set max limit of freelancers to be displayed
 const maxDisplay = 6;
 
 const state = {
-    freelncers: [
+    freelancers: [
     {
         name: "Alice",
         occupation: "Writer",
@@ -30,35 +30,61 @@ const state = {
 // === Script ===
 render(state);
 
+const freelancerInterval = setInterval(function (){
+    addFreelancer(state.freelancers);
+    if (state.freelancers.length >= maxDisplay) {
+        clearInterval(freelancerInterval);
+    }
+
+    updateAveragePrice(state);
+    render(state);
+}, 1000);
+
 // === Functions ===
 function render(state) {
     renderFreelancers(state.freelancers);
-    renderAvgPrice(state.averagePrice);
+    renderAveragePrice(state.averagePrice);
 }
+
 function renderFreelancers(freelancers) {
-    const alias = document.querySelector("name");
-    const aliasElements = freelancers.map((freelancers) =>{
+    const alias = freelancers.map((freelancer) =>{
         const element = document.createElement("li");
-        element.classList.add(freelancers.name);
+        const aliasLi = document.createElement("li");
+
+        //aliasLi.IneerHTML = 
+        aliasLi.textContent = freelancer.name;
+        element.append(aliasLi);
+
+        const jobLi = document.createElement("li");
+        jobLi.textContent = freelancer.occupation;
+        element.append(jobLi);
+
+        const rateLi = document.createElement("li");
+        rateLi.textContent = freelancer.price;
+        element.append(rateLi);
+
         return element;
     });
-    alias.replaceChildren(...aliasElements);
 
-    const job = document.querySelector("occupation");
-    const jobElements = freelancers.map((freelancers) =>{
-        const element = document.createElement("li");
-        element.classList.add(freelancers.occupation);
-        return element;
-    });
-    job.replaceChildren(...jobElements);
+    const sec = document.querySelector(".name");
+    sec.replaceChildren(...freelancers);
 
-
-    const rate = document.querySelector("startPrice");
-    const rateElements = freelancers.map((freelancers) =>{
-        const element = document.createElement("li");
-        element.classList.add(freelancers.price);
-        return element;
-    });
-    rate.replaceChildren(...rateElements);
 }
 
+function renderAveragePrice(price) {
+    const rate = document.querySelector(".averagePrice");
+    rate.textContent = `$${price.toFixed(2)}`;
+}
+
+function addFreelancer(freelancers) {
+    const name = names[Math.floor(Math.random() * names.length)];
+    const occupation = occupations[Math.floor(Math.random() * occupations.length)];
+    const price = 1 + Math.floor(Math.random() * 100);
+
+    freelancers.push({name, occupation, price});
+}
+
+function updateAveragePrice(state) {
+    const sum = state.freelancers.reduce((acc, freelancer) => acc + freelancer.price, 0);
+    state.averagePrice = sum / state.freelancers.length;
+}
